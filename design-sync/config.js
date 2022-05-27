e76c12138ca28700e4bb64332996a8547f7b4111
yoonsun lee <yoonsunlee150@gmail.com>
2022-05-27 19:12:10 -0400

    portfolio commit

---
+++ design-sync/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  site: `https://www.ysl150.com/`,
+  pages: [],
+  clientRouting: true,
+  optimizeImages: true,
+  optimizeJsLoading: true,
+  removeBranding: true,
+  minifyJs: false,
+  staticPageLimit: 200,
+  revalidate: false,
+}
